c6478e3b988c16a4ded5c745d0e6aeb0b2c70449
qadritanzeel1 <tanzeelqadri042@gmail.com>
2025-12-03 17:25:44 +0530

    implicit return

--- day 10/script.js
+++ day 10/script.js
@@ -2,13 +2,34 @@
 
 //Arrow Functions
 
-let sum = (a,b)=>{
-    let nstr=""
- for(let i = 1;i<=b;i++){
-    nstr = nstr +a
- }
- return nstr
+let sum = (a, b) => {
+   let nstr = ""
+   for (let i = 1; i <= b; i++) {
+      nstr = nstr + a
+   }
+   return nstr
 }
 
-console.log(sum("string",5))
+console.log(sum("string", 5))
+
+
+//implicit return
+
+// let add = (a,b) => a*b
+
+// console.log(add(6,9))
+
+// fn greeting => (name,timePeriod) =>"night" =>name + Good night
+
+// m//e//a//n
+
+let greeting = (name, timePeriod) => timePeriod == "m" ? "Good Morning " + name
+   : timePeriod == "e" ? "Good Evening " + name
+      : timePeriod == "a" ? "Good Afternoon " + name
+         : timePeriod == "n" ? "Good Night " + name
+            : "invalid input"
+
+console.log(greeting("Tanzeel" , "t")   )     ;
+
+
 
