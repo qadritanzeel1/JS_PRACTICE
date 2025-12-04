52c87f5f8b113b10d3f969ffadaa6cf005e538df
qadritanzeel1 <tanzeelqadri042@gmail.com>
2025-12-04 13:04:25 +0530

    Global Executional Context

---
+++ day 11/script.js
@@ -0,0 +1,5 @@
+//Global executional context
+
+// var is fully hoisted, while let and const are hoisted but stay in the Temporal Dead Zone (TDZ) until initialization.
+
+//Global Execution Context (GEC) in JavaScript is the first and default execution environment where your entire JavaScript program starts running.
