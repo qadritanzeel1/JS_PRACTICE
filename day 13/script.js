6b65aaaf3f166188c70628dc5440d3e2b99b174a
qadritanzeel1 <tanzeelqadri042@gmail.com>
2025-12-31 10:54:17 +0530

    cllosures

--- day 13/script.js
+++ day 13/script.js
@@ -45,3 +45,68 @@ console.log(greet1("night"))
 
 //Encapsulation:Encapsulation means hiding internal data and allowing access only through controlled methods
 //In JavaScript, encapsulation is achieved by hiding variables using closures or private class fields
+
+
+// function counter(){
+//     let count = 0;
+//     return function(){
+//         count++
+//         return count
+//     }
+// }
+
+// let count1 = counter()
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())
+// console.log(count1())  // it will increase the count 
+
+// we cant have multiple return statements in fxns (without conditions)
+// may be we can assign a data structure to retuen statement and in that data structure return multiple values .
+
+
+function counter(){
+    let count =0;
+    return {
+        increment : function(){
+            count++
+            return count
+        }, 
+        decrement : function(){
+            count--;
+            return count;
+        },
+    }
+}
+
+let counterSetup = counter();
+console.log(counterSetup.increment())
+console.log(counterSetup.increment())
+console.log(counterSetup.increment())
+console.log(counterSetup.increment())
+console.log(counterSetup.increment())
+console.log(counterSetup.increment())   
+console.log(counterSetup.decrement())
+console.log(counterSetup.decrement());
+
+
+
+function createQuize(q,a){
+        let attemp =0;
+
+        return function(userAns){
+            attemp++
+          if(userAns ==a){
+            return true + ` no of attempts done ${attemp}  `
+          }else{
+            return false  + ` no of attempts done ${attemp}  `
+          }
+        }
+}
+
+let quize1 = createQuize("what is the capital of India","Delhi");
+console.log(quize1("Delhip"))
+console.log(quize1("Delhip")) //the attempt score will be incresing 
