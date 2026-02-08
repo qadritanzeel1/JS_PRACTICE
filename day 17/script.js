814f8b9fab36fa27d04cf9e9ced77c5942f311ea
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-02-08 22:17:13 +0530

    new exercise

---
+++ day 17/script.js
@@ -0,0 +1,14 @@
+let key = "city";
+let user = {
+  name: "Ali",
+  city: "Mumbai"
+};
+
+// 👉 Print the city using the variable key
+
+console.log(user[key])
+
+let obj = {
+  "first name": "Ayaan"
+};
+console.log(obj["first name"]);
