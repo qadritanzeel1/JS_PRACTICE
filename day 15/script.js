3707a80199495d4dca87a414a75a59c694445b51
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-02-08 19:59:35 +0530

    object task 1

---
+++ day 15/script.js
@@ -0,0 +1,18 @@
+// Create an object student with:
+
+// name
+
+// roll
+
+// marks
+
+// Print all three values.
+
+let student ={
+    name: "Tanzeel Ul Huda ",
+    roll:12,
+    marks:48
+
+}
+
+console.log(student)
